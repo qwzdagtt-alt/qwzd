9470a74a7682ff1860884cebbc0a2a64a7fa0d3e
qwzdagtt-alt <qwzdagtt@gmail.com>
2026-08-20 15:51:35 +0900

    Add files via upload

---
+++ Day01/Swap.java
@@ -0,0 +1,16 @@
+package Swap;
+
+import java.util.Scanner;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(10);
+		int a = 10;
+		System.out.println(25);
+		int b = 25;
+		int temp = a;
+		
+	}
+}
